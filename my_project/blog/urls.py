de1f42b99cb125d33504c0357869b008f9a290c2
ahmadkybora <ahmadkybora@gmail.com>
2022-08-19 14:01:08 +0430

    ..

--- my_project/blog/urls.py
+++ my_project/blog/urls.py
@@ -6,3 +6,8 @@ urlpatterns = [
     path('', home, name="home"),
     path('api/', api, name="api")
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
