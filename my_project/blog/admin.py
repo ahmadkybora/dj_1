2eca93272e66af2ae8d24a396732f8c721cf75ba
ahmadkybora <ahmadkybora@gmail.com>
2022-08-26 16:14:55 +0430

    ..

--- my_project/blog/admin.py
+++ my_project/blog/admin.py
@@ -1,5 +1,11 @@
+from re import search
 from django.contrib import admin
 from .models import Article
 # Register your models here.
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'publish', 'status')
+    list_fiter = ('publish', 'status')
+    search_filter = ('title', 'description')
+    prepopulated_fields = {'slug': ('title')}
 admin.site.register(Article)
