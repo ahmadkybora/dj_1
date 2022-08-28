4fd47dfd9c2df38826f91a1b597c7d66fe3e073e
ahmadkybora <ahmadkybora@gmail.com>
2022-08-28 15:53:13 +0430

    ..

--- my_project/blog/views.py
+++ my_project/blog/views.py
@@ -1,35 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
+from .models import Article
+
 # Create your views here.
 def home(request):
+    # context = {
+    #     "articles": [
+    #         {
+    #             "id": 1,
+    #             "title": "ss", 
+    #             "desc": "ss",
+    #             "img": "s"
+    #         },
+    #         {
+    #             "id": 2,
+    #             "title": "ss", 
+    #             "desc": "ss",
+    #             "img": "s"
+    #         },
+    #         {
+    #             "id": 3,
+    #             "title": "ss", 
+    #             "desc": "ss",
+    #             "img": "s"
+    #         },
+    #         {
+    #             "id": 4,
+    #             "title": "ss", 
+    #             "desc": "ss",
+    #             "img": "s"
+    #         }
+    #     ]
+    # }
     context = {
-        "articles": [
-            {
-                "id": 1,
-                "title": "ss", 
-                "desc": "ss",
-                "img": "s"
-            },
-            {
-                "id": 2,
-                "title": "ss", 
-                "desc": "ss",
-                "img": "s"
-            },
-            {
-                "id": 3,
-                "title": "ss", 
-                "desc": "ss",
-                "img": "s"
-            },
-            {
-                "id": 4,
-                "title": "ss", 
-                "desc": "ss",
-                "img": "s"
-            }
-        ]
+        # "articles": Article.objects.all() 
+        "articles": Article.objects.filter(status="p").order_by("-publish")[:3] 
     }
     return render(request, "blog/home.html", context)
 
