41ae684e070322e6224c267aaf33fa28c0d0e49b
ahmadkybora <ahmadkybora@gmail.com>
2022-09-02 17:40:40 +0430

    ...

--- my_project/blog/models.py
+++ my_project/blog/models.py
@@ -1,20 +1,24 @@
-# from django.db import models
-# from django.utils import timezone
+from platform import version
+from django.db import models
+from django.utils import timezone
 
-# class Article(models.Model):
-#     STATUS_CHOICES = (
-#         ('d', 'Draft'),
-#         ('p', 'Published')
-#     )
-#     title = models.CharField(max_length=200)
-#     slug = models.SlugField(max_length=100, unique=True)
-#     description = models.TextField()
-#     thumbnail = models.ImageField(upload_to='images')
-#     publish = models.DateTimeField(default=timezone.now)
-#     created = models.DateTimeField(auto_now_add=True)
-#     updated = models.DateTimeField(auto_now=True)
-#     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+class Article(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published')
+    )
+    title = models.CharField(max_length=200, verbose_name="عنوان")
+    slug = models.SlugField(max_length=100, unique=True, verbose_name="آدرس مقاله")
+    description = models.TextField(verbose_name="توضیحات")
+    thumbnail = models.ImageField(upload_to='images',  verbose_name="تصویر")
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
 
+class Meta:
+    verbose_name = "مقاله"
+    verbose_name_plural = "مقالات" 
 class User():
     pass
 
